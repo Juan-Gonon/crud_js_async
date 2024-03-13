2b379363b6319c5254c152ad2d799fce27b803ac
Juan Gonon <juanfegonon27@gmail.com>
2024-03-12 23:59:17 -0600

    create client

--- service/client-service.js
+++ service/client-service.js
@@ -3,9 +3,24 @@ const listCliente = () => {
   return fetch('http://localhost:3000/perfil').then((response) => response.json())
 }
 
+const crearCliente = (nombre, email)=>{
+  return fetch('http://localhost:3000/perfil',{
+    method: "POST",
+    headers:{
+      "Content-type":"application/json"
+    },
+    body: JSON.stringify({
+      nombre,
+      email,
+      id: uuid.v4()
+    })
+  })
+}
+
 
 export const clientServices = {
-  listCliente
+  listCliente,
+  crearCliente
 }
 
 
